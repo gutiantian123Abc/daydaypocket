434b9d139bff8aa15a40915a46589e16d2b75d6e
gutiantian123Abc <william.guxiangtian@gmail.com>
2018-06-27 19:29:32 -0700

    Add com.yarolegovich dependency

--- app/src/main/java/com/daypocket/xiangtiangu/daydaypocket/MainActivity.java
+++ app/src/main/java/com/daypocket/xiangtiangu/daydaypocket/MainActivity.java
@@ -2,6 +2,23 @@ package com.daypocket.xiangtiangu.daydaypocket;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.app.Fragment;
+import android.content.res.TypedArray;
+import android.graphics.drawable.Drawable;
+import android.os.Bundle;
+import android.support.annotation.ColorInt;
+import android.support.annotation.ColorRes;
+import android.support.annotation.Nullable;
+import android.support.v4.content.ContextCompat;
+import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.LinearLayoutManager;
+import android.support.v7.widget.RecyclerView;
+import android.support.v7.widget.Toolbar;
+
+import com.yarolegovich.slidingrootnav.SlidingRootNav;
+import com.yarolegovich.slidingrootnav.SlidingRootNavBuilder;
+import java.util.Arrays;
+
 
 public class MainActivity extends AppCompatActivity {
 
